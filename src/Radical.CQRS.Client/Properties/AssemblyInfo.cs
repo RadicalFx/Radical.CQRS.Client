ebd1dae02b3a4fdca92a3755d19725fea089addf
Mauro Servienti <mauro@topics.it>
2016-03-13 00:51:40 +0100

    adding GitVersion

--- src/Radical.CQRS.Client/Properties/AssemblyInfo.cs
+++ src/Radical.CQRS.Client/Properties/AssemblyInfo.cs
@@ -11,12 +11,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1d1b36e0-f47f-4802-9f58-44f841b13488")]
-[assembly: AssemblyVersion( Consts.version )]
-[assembly: AssemblyFileVersion( Consts.version )]
-[assembly: AssemblyInformationalVersion( Consts.version + Consts.preRelease )]
-
-class Consts
-{
-	public const string version = "0.1.1";
-	public const string preRelease = "-RC1";
-}
